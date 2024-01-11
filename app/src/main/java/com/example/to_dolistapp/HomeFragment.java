0e4a3693cb76cf675702a1b570ced321c0796044
AkshitCode <as1715@srmist.edu.in>
2024-01-11 06:44:03 +0530

    .

--- app/src/main/java/com/example/to_dolistapp/HomeFragment.java
+++ app/src/main/java/com/example/to_dolistapp/HomeFragment.java
@@ -34,14 +34,6 @@ import java.util.ArrayList;
 public class HomeFragment extends Fragment implements TaskRecyclerAdapter.TaskIsClickedInterface {
 
 
-//    @Override
-//    public void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        if (getArguments() != null) {
-//            mParam1 = getArguments().getString(ARG_PARAM1);
-//            mParam2 = getArguments().getString(ARG_PARAM2);
-//        }
-//    }
 
     private RecyclerView recyclerView;
     private ToDoTaskDatabase toDoTaskDatabase;
